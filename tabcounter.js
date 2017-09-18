d13b0473314ff148146f00b60c8a3fa4dd3b26f7
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2017-09-18 16:57:48 +0200

    Fix tab counting when moving tab between windows

--- tabcounter.js
+++ tabcounter.js
@@ -4,17 +4,19 @@ var tabRemoved = false;
 
 browser.browserAction.setBadgeBackgroundColor({color: "gray"});
 browser.tabs.onActivated.addListener(refreshCounter);
-browser.tabs.onCreated.addListener(refreshCounter);
-browser.tabs.onAttached.addListener(refreshCounter);
 browser.tabs.onRemoved.addListener(() => tabRemoved = true);
 
-refreshCounter();
+initCounter();
 
-function refreshCounter() {
-  browser.tabs.query({currentWindow: true}).then(updateCounter, onError);
+function initCounter() {
+  browser.tabs.query({currentWindow: true}).then(tabs => updateCounter(getActiveTabId(tabs), tabs), onError);
 }
 
-function updateCounter(tabs) {
+function refreshCounter(tabInfo) {
+  browser.tabs.query({windowId: tabInfo.windowId}).then(tabs => updateCounter(tabInfo.tabId, tabs), onError);
+}
+
+function updateCounter(tabId, tabs) {
   var tabCount = tabs.length;
 
   // XXX because a removed tab is still part of the "tabs" array at this time, we have to adjust the counter
@@ -23,11 +25,11 @@ function updateCounter(tabs) {
     tabRemoved = false;
   }
 
-  browser.browserAction.setBadgeText({text: tabCount.toString(), tabId: getActiveTabId(tabs)});
+  browser.browserAction.setBadgeText({text: tabCount.toString(), tabId: tabId});
 }
 
 function getActiveTabId(tabs) {
-  return tabs.filter((tab) => tab.active)[0].id;
+  return tabs.filter(tab => tab.active)[0].id;
 }
 
 function onError(error) {
